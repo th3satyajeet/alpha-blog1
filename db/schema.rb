ef404f18da5343df190a31926bdf070a4b5b5139
Satyajeet Nayak <satyajeetnayak2015@gmail.com>
2019-07-30 11:54:47 +0530

    Add secure pw and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_104441) do
+ActiveRecord::Schema.define(version: 2019_07_30_062032) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_104441) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
