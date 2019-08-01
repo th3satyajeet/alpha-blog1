4638e24f7cc8ad6c8a2d99d332e438a5e02a06b0
Satyajeet Nayak <satyajeetnayak2015@gmail.com>
2019-08-01 16:50:46 +0530

    Update all the pages look

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -87,15 +87,15 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-    
-    if current_user != @article.user
-    
-    flash[:danger] = "You can only edit or delete your own articles"
-    
-    redirect_to root_path
-    
+
+        if current_user != @article.user and !current_user.admin?
+        
+            flash[:danger] = "You can only edit or delete your own articles"
+            
+            redirect_to root_path
+        
+        end
+        
     end
     
-    end
-    
-    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_062032) do
+ActiveRecord::Schema.define(version: 2019_08_01_090206) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_062032) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
